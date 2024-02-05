19f17374ede84c05551f479f274a80a846413370
panda21127 <94950215+panda21127@users.noreply.github.com>
2024-02-05 20:49:54 +0000

    Lab_1

--- Lab_1/tasks.erl
+++ Lab_1/tasks.erl
@@ -1,6 +1,6 @@
 -module(tasks). % Вариант 17
 
--export([ball_volume/1 , from_to/2, delta/1, int_to_binary/1, rle_encode/1]).
+-export([ball_volume/1 , from_to/2, delta/1, int_to_binary/1, rle_encode/1,intersect/2,is_date/3]).
 
 -import(math,[pi/0]).
 
@@ -33,24 +33,71 @@ int_to_binary(N) when N >= 0->
 
 
 rle_encode([]) -> 0;
-rle_encode(List) -> rle_encode(List, [], 0).
-rle_encode([], Symbol, N) -> 0;
-rle_encode([Head | Tail], Symbol, N) ->
-     if 
-        N>-1 ->
-        io:format("~w~n",[is_binary(Symbol)]),
-        io:format("~w~n",[is_atom(Symbol)]),
-        io:format("~w~n",[is_float(Symbol)]),
-        io:format("~w~n",[is_integer(Symbol)]),
-        io:format("~w~n~n",[is_list(Symbol)]),
-        io:format("~w~n~n",[string:equal(Symbol, [Head])]),
-        rle_encode(Tail, [Head], N+1);
-     true ->
-        if
-            N==0 ->
-                rle_encode(Tail, Head, 0); %string:join([String,",",Head],"")
-            true->
-                rle_encode(Tail, Head, 0) %string:join([String,",{",Head,",",N,"}"],"")
-        end
+rle_encode([Head | Tail]) -> rle_encode([Head | Tail], Head , [] , 0).
+rle_encode([], Symbol,FinishList, N) -> 
+    if
+        N == 1 ->
+            NewList= Symbol;
+        true ->
+            NewList= {Symbol,N}
+    end,
+    lists:reverse([NewList | FinishList]);
+rle_encode([Head | Tail], Symbol, FinishList, N) ->
+    if 
+        Symbol == Head->
+            rle_encode(Tail, Head,FinishList, N+1);
+        true ->
+            if
+                N == 1 ->
+                    NewList= Symbol,
+                    rle_encode(Tail, Head,[NewList|FinishList], 1); %string:join([String,",",Head],"")
+                true->
+                    NewList= {Symbol,N},
+                    rle_encode(Tail, Head,[NewList|FinishList], 1) %string:join([String,",{",Head,",",N,"}"],"")
+            end
      end.
 
+
+
+     intersect(List1, List2)-> (List1 -- (List1 -- List2)).  % Task_6
+
+
+
+     is_date(DayOfMonth, MonthOfYear, Year)->is_date(DayOfMonth, MonthOfYear, Year,6).
+     is_date(0, MonthOfYear, Year,Day)->
+         case MonthOfYear of
+             MonthOfYear when MonthOfYear == 2;
+                              MonthOfYear == 4;
+                              MonthOfYear == 6;
+                              MonthOfYear == 8;
+                              MonthOfYear == 9;
+                              MonthOfYear == 11;
+                              MonthOfYear == 1->
+                 is_date(31, MonthOfYear-1, Year,Day);
+             MonthOfYear when MonthOfYear == 5;
+                              MonthOfYear == 7;
+                              MonthOfYear == 10;
+                              MonthOfYear == 12->
+                 is_date(30, MonthOfYear-1, Year,Day);
+             MonthOfYear when MonthOfYear == 3->
+                 if
+                     (((Year rem 4) == 0) and ((Year rem 100) /= 0)) or ((Year rem 400) == 0)->
+                         is_date(29, MonthOfYear-1, Year,Day);
+                     true->
+                         is_date(28, MonthOfYear-1, Year,Day)
+                 end                                                                                             % Task_7
+             end;
+     is_date(_, 0, Year,Day)->is_date(31, 12, Year-1,Day);
+     is_date(DayOfMonth, MonthOfYear, Year,8)->is_date(DayOfMonth, MonthOfYear, Year,1);
+     is_date(1, 1, 2000,Day)->
+         case Day of
+            Day when Day == 1 -> io:format("Понедельник~n");
+		    Day when Day == 2 -> io:format("Вторник~n");
+		    Day when Day == 3 -> io:format("Среда~n");
+		    Day when Day == 4 -> io:format("Четверг~n");
+		    Day when Day == 5 -> io:format("Пятница~n");
+		    Day when Day == 6 -> io:format("Суббота~n");
+		    Day when Day == 7 -> io:format("Воскресенье~n")
+         end;
+     is_date(DayOfMonth, MonthOfYear, Year,Day)-> is_date(DayOfMonth - 1 , MonthOfYear, Year,Day+1).
+     
