91ecd4ea5bb4db66f363707518435fc3dbf7f20f
panda21127 <94950215+panda21127@users.noreply.github.com>
2024-04-08 06:56:51 +0000

    Parent - child

--- Lab4/parent_children.erl
+++ Lab4/parent_children.erl
@@ -1,73 +1,58 @@
 -module(parent_children).
--export([start/1,send_to_child/3,stop/1,main/0,par_filter/3]).
+-export([start/1]).
 -compile(export_all).
 
-start_children()->
-	spawn(parent_children, children,[]).
-start_parent()->
-	spawn(parent_children, parent,[]).
+start_child(Pid)->
+	spawn(parent_children,child,[Pid]).
+
+start_child(Pid,N)->
+	start_child(Pid,N,[]).
+
+start_child(_,0,List)->List;
+start_child(FPid,N,List)->
+	Pid = start_child(FPid),
+	start_child(FPid,N-1,[Pid | List]).
+
+send_child([],_,_,_)->'ok';
+send_child([H|_],Message,Listchild,0)->H ! {'work',Message,Listchild};
+send_child([_|T],Message,Listchild,N)->
+	send_child(T,Message,Listchild,N-1).
+
+stop([])->'ok';
+stop([H|T])->
+	H ! {'finish'},
+	stop(T).
 
 start(N)->
-	Pid = start_parent(),
-	List = creatChildren(N,[]),
-	[Pid,List].
-	
-creatChildren(0,List)->List;
-creatChildren(N,List)->
-	Pid = start_children(),
-	creatChildren(N-1,[Pid|List]).
+	Pid = spawn(parent_children,main,[[]]),
+	Pid ! {'send','Finally',N}.
 
-search(1,[H|_])->H;
-search(I,[_|T])->
-	search(I-1,T).
-
-send_to_child(I, Msg,List)->
-	[H,T]=List,
-	IPid=search(I,T),
-	H ! {'task',Msg,IPid}.
-stop([H,T])-> H ! {'die',T}.
-
-
-children()->
+main(ListCheck)->
 	receive
-		{'stop'}->
-			io:format("Hi! I am stop~n");
-		{'die'}->
-			io:format("Hi! I am child ~w and I am die now~n",[self()]),
-			'error';
-		{'work',Msg}-> 
-			io:format("Hi!~w get message: ~w~n",[self(),Msg]),
-			children()
+		{'finish'}->
+			io:format("FINISH~n"),
+			stop(ListCheck),
+			exit(self(),kill);
+		{'send',Message,N}->
+			Listchild = start_child(self(),N),
+			send_child(Listchild,Message,Listchild,2),
+			main(Listchild);
+		{'send_again',Message}->
+			send_child(ListCheck,Message,ListCheck,3),
+			main(ListCheck);
+		{'get',Message,PID}->
+			io:format("~w~n",[ListCheck]),
+			io:format("Get ~w  from  ~w~n",[Message,PID]),
+			self() ! {'finish'},
+			main(ListCheck)
 	end.
-parent()->
-	receive	
-		{'die',List}->
-			send_children_to_die(List),
-			io:format("Hi! I am father and I am die now ~n");
-		{'die',I,List}-> 
-			IPid = search(I,List),
-			io:format("Hi! I am father and ~w you are die for me ~n",[IPid]),
-			IPid ! {'die'},
-			Nlist = List--[IPid],
-			NPid = start_children(),
-			io:format("Hi! I am father and ~w you are new child for me ~n",[NPid]),
-			[NPid|Nlist];
-		{'task',Msg,IPid}-> 
-			io:format("Hi!Children is ~w~n",[IPid]),
-			IPid ! {'work',Msg}
-	end,
-	parent().
-	
-send_children_to_die([])->'ok';
-send_children_to_die([H|T])->
-	H ! {'die'},
-	send_children_to_die(T).
-	
-main()->
-	List=start(6),
-	[H,T]=List,
-	send_to_child(3, 'Hi',List),
-	Somt = H!{'die',2,T},
-	io:format("~w~n",[Somt]),
-	stop(List).
-	
+child(Pid)->
+	receive
+		{'work',Message,Listchild}->
+			io:format("Hello is ~w  ~w  ~n",[self(),Message]),
+			Pid ! {'get',Message,self()},
+			child(Pid);
+		{'finish'}->
+			io:format("finish  ~w~n",[self()]),
+			exit(self(),kill)
+	end.
